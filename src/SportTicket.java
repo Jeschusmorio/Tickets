ef326a329178680e951a2b52b9d9534d855ac4e4
Jeschusmorio <mpetrovic1@tsn.at>
2020-01-14 22:48:22 +0100

    modified the abstract version into a interface version

--- src/SportTicket.java
+++ src/SportTicket.java
@@ -1,5 +1,11 @@
 
-public class SportTicket extends Ticket {
+public class SportTicket implements Ticket {
+	private String eventLocation;
+	private String eventName;
+	private String date;
+	private double basicPrice;
+	private double ticketPrice;
+	
 	public SportTicket(String eventLocation, String eventName, String date, double basicPrice) {
 		setEventLocation(eventLocation);
 		setEventName(eventName);
@@ -29,5 +35,44 @@ public class SportTicket extends Ticket {
 		else {
 			setTicketPrice(getBasicPrice());
 		}
+	}
+	public void setEventLocation(String eventLocation) {
+		this.eventLocation = eventLocation;
+	}
+	public String getEventLocation() {
+		return eventLocation;
+	}
+	public void setEventName(String eventName) {
+		this.eventName = eventName;
+	}
+	public String getEventName() {
+		return eventName;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setBasicPrice(double basicPrice) {
+		this.basicPrice = basicPrice;
+	}
+	public double getBasicPrice() {
+		return basicPrice;
+	}
+	public void setTicketPrice(double ticketPrice) {
+		this.ticketPrice = ticketPrice;
+	}
+	public double getTicketPrice() {
+		return ticketPrice;
+	}
+	public void output() {
+		System.out.println();
+		System.out.println("+-----------------------------------------------v-------------+");
+		System.out.println("|\t\t"+eventLocation+"\t\t\t|             |");
+		System.out.println("|\t\t"+eventName+"\t\t\t|             |");
+		System.out.println("+-----------------------+-----------------------+             |");
+		System.out.println("|\t"+date+"\t|\t"+ticketPrice+" €\t\t|             |");
+		System.out.println("+-----------------------------------------------^-------------+");
 	}
 }
